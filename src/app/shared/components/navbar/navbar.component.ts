69bffc5d3eda46c9735c00a644e4e143b07e9a13
Kelvin Joel Carreño Espin <kcarrenoespin@gmail.com>
2023-11-22 22:12:47 +0100

    Creando estilos globales y mejorando routing

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -8,6 +9,16 @@ import { CommonModule } from '@angular/common';
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit{
+  constructor(private route: ActivatedRoute) {}
 
+  ngOnInit() {
+    this.route.fragment.subscribe((fragment: any) => {
+      console.log('fragmaen', fragment)
+      if (fragment) {
+        const element = document.querySelector(`#${fragment}`);
+        if (element) element.scrollIntoView({ behavior: 'smooth' });
+      }
+    });
+  }
 }
